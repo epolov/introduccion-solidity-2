5d0fc0b75ddb7ddcb21f906be62f771dac259bcb
IvánManso <imanso.arenal@gmail.com>
2023-12-11 20:58:44 +0100

    Volumen 01 => RegistroDeEmpleadosBM.sol con esteroides añadido

--- 01. Ejercicios/01. Volumen 1/RegistroDeEmpleadosBM.sol
+++ 01. Ejercicios/01. Volumen 1/RegistroDeEmpleadosBM.sol
@@ -10,15 +10,28 @@ contract RegistroDeEmpleadosBM {
         string nombre;
         uint256 salario;
     }
-    // Empleado(35, "AR", 25000)
 
     mapping(uint256 => Empleado) public empleados;
-    // 45 => Empleado(0, "", 0)
-    // 35 => Empleado(35, "AR", 25000)
-    // empleados[35]; // Empleado(35, "AR", 25000)
-    // empleados[35].idEmpleado; // 35
+
+    event EmpleadoAgregado(uint256 indexed idEmpleado, string nombre, uint256 salario);
+    event EmpleadoEliminado(uint256 indexed idEmpleado);
+    event SalarioModificado(uint256 indexed idEmpleado, uint256 salario);
+
+    error ErrorNoEresPropietario();
+
+    modifier soloPropietario() {
+        require(propietario == msg.sender, "Error: no eres el propietario");
+        // continue
+        _;
+    }
+
+    modifier empleadoExiste(uint256 _idEmpleado) {
+        require(empleados[_idEmpleado].idEmpleado != 0, "Error: el empleado no existe");
+        _;
+    }
 
     constructor() {
+        // msg.sender / msg.value / block.timestamp != pure
         propietario = msg.sender;
     }
 
@@ -26,37 +39,39 @@ contract RegistroDeEmpleadosBM {
         uint256 _idEmpleado,
         string memory _nombre,
         uint256 _salario
-    ) public {
-        // comprobar que el emisor es el propietario
-        require(propietario == msg.sender, "Error: no eres el propietario");
-        // 2a condición
+    ) public soloPropietario {
         require(empleados[_idEmpleado].idEmpleado == 0, "Error: el empleado ya existe");
-        // Agregar nuevo empleado al mapping
         empleados[_idEmpleado] = Empleado(
             _idEmpleado,
             _nombre,
             _salario
         );
+
+        // emitir evento EmpleadoAgregado(idEmpleado, nombre, salario)
+        emit EmpleadoAgregado(_idEmpleado, _nombre, _salario);
     }
 
-    function obtenerEmpleado(uint256 _idEmpleado) public view returns(
+    function obtenerEmpleado(uint256 _idEmpleado) public view empleadoExiste(_idEmpleado) returns(
         uint256,
         string memory,
         uint256
-    ){
-        // Condición: Comprobar si el empleado existe
-        // TO-DO
-
-        // 1a opción: devolver datos del empleado
-        // Empleado memory elEmpleado = empleados[_idEmpleado]; // Empleado(35, "AR", 25000)
-        //return (elEmpleado.idEmpleado, elEmpleado.nombre, elEmpleado.salario);
-        
-        // 2a opción: devolver datos del empleado
+    ) {
         return(
             empleados[_idEmpleado].idEmpleado,
             empleados[_idEmpleado].nombre,
             empleados[_idEmpleado].salario
         );
+    }
+
+    function actualizarSalarioEmpleado(
+        uint256 _idEmpleado,
+        uint256 _salario
+    ) public soloPropietario empleadoExiste(_idEmpleado) {
+        empleados[_idEmpleado].salario = _salario; // Mapping => Struct => Campos
+    }
+
+    function eliminarEmpleado(uint256 _idEmpleado) public soloPropietario empleadoExiste(_idEmpleado) {
+        delete empleados[_idEmpleado];
     }
 
 }
